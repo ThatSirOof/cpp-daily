a1b7707e0afa835d9735c6eb10097fedff45db82
ThatSirOof <friednoodles12@gmail.com>
2026-05-26 00:47:40 -0500

    Program manage store product by calculating prices, applying discounts and updating inventory

--- testing.cpp
+++ testing.cpp
@@ -1,79 +1,64 @@
 #include <iostream>
 #include <math.h>
+#include <string>
 
-struct Point2D{
-    double x{};
-    double y{};
+struct Product{
+    std::string name{};
+    double price{};
+    double quantity{};
 };
 
-void print_point(const Point2D& coordinate);
-void print_distance(double distance);
-void move_point(Point2D& coordinate, int dx, int dy);
-
-double calculate_distance(const Point2D& first_set, const Point2D& second_set);
-Point2D midpoint(const Point2D& first_set, const Point2D& second_set);
+void print_product(const Product& information);
+double calculate_total_price(const Product& item, double discount);
+int update_quantity(Product& item, int new_quantity);
+double increase_price(Product& item, double increase);
 
 int main(){
 
-    Point2D point_1{};
-    Point2D point_2{};
+    double discount{0.15};
+    double upcount{};
+    int quantity{};
+    Product vegetable{};
 
-    int x_value{}, y_value{};
+    std::cout << "Pick a vegetable, then give it a price, and a quantity: ";
+    std::cin >> vegetable.name >> vegetable.price >> vegetable.quantity;
+    print_product(vegetable);
 
-    std::cout << "Choose two different coordinate(x,y): ";
-    std::cin >> point_1.x >> point_1.y >> point_2.x >> point_2.y;
+    std::cout << "The total with a discount is: " << calculate_total_price(vegetable, discount) << std::endl;
 
-    std::cout << "The first coordinate are: ";
-    print_point(point_1);
-    std::cout << "The second coordinate are: ";
-    print_point(point_2);
+    std::cout << "\nHow many more quantity would you like to stock more?: ";
+    std::cin >> quantity;
+    std::cout << "The new quantity is: " << update_quantity(vegetable, quantity) << std::endl;
 
-    std::cout << "The calculated distances are: ";
-    calculate_distance(point_1, point_2);
+    std::cout << "\nHow many percentage do you want to increase the price?: ";
+    std::cin >> upcount;
+    std::cout << "The new price is: " << increase_price(vegetable, upcount) << std::endl;
 
-    std::cout << "Pick two numbers(x, y) to modify the coordinates: ";
-    std::cin >> x_value >> y_value;
-    move_point(point_1, x_value, y_value);
-    move_point(point_2, x_value, y_value);
-    
-    std::cout << "New first coordinates are: ";
-    print_point(point_1);
-    std::cout << "New second coordinates are: ";
-    print_point(point_2);
-
-    std::cout << "The midpoint of the coordinates are: ";
-    print_point(midpoint(point_1, point_2));
-    
     return 0;
 }
 
-void print_point(const Point2D& coordinate){
-    std::cout << coordinate.x << ", " << coordinate.y << std::endl;
+void print_product(const Product& information){
+    std::cout << 
+    "This is a " << information.name << 
+    " and it is worth $" << information.price <<
+    " with " << information. quantity << " in stock." << std::endl;
+}
+
+double calculate_total_price(const Product& item, double discount){
+    double total{item.price};
+
+    total -= item.price * discount;
+    return total;
+}
+
+int update_quantity(Product& item, int new_quantity){
+    return item.quantity += + new_quantity;
+}
+
+double increase_price(Product& item, double increase){
+    return item.price += item.price * (increase / 100.0);
 }
 
 
-double calculate_distance(const Point2D& first_set, const Point2D& second_set){
-    double distance = sqrt(pow(second_set.x - first_set.x, 2) + pow(second_set.y - first_set.y, 2));
 
-    print_distance(distance);
 
-    return distance;
-}
-
-void print_distance(double distance){
-    std::cout << distance << std::endl;
-}
-
-void move_point(Point2D& point, int dx, int dy){
-    point.x += dx;
-    point.y += dy;
-}
-
-Point2D midpoint(const Point2D& first_set, const Point2D& second_set){
-    Point2D mid{};
-
-    mid.x = (first_set.x + second_set.x) / 2;
-    mid.y = (first_set.y + second_set.y) / 2;
-
-    return mid;
-}
